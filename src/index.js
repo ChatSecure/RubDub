b9beef56c556f3599be3b36815ab10b7bbafd5fd
David Chiles <david@davidchiles.org>
2016-06-09 14:44:40 -0700

    Added support for command line arguments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 var xmpp = require('./xmpp.js');
 var api = require('./api.js');
+var program = require('commander');
 
-var port = 5269;
-var bindAddress = '';
-var domain = '';
+
+program
+  .version('0.0.2')
+  .option('-p, --port <n>', 'The server port number', parseInt)
+  .option('-b, --bindAddress <value>', 'The server bind address')
+  .option('-d, --domainAddress <value>', 'The server domain')
+  .option('-k, --keyPath <path>', 'The path of the TLS key')
+  .option('-c, --certPath <path>', 'The path of the TLS certificate')
+  .parse(process.argv);
+
+var port = program.port || 5269;
+var bindAddress = program.bindAddress;
+var domain = program.domainAddress;
+var tls = {};
+tls.keyPath = program.keyPath;
+tls.certPath = program.certPath;
 var options = {
-
+  tls:tls
 };
 
 var xServer = new xmpp.xmppServer();
